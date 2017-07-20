6b158a955b1116f0a9ff0bbd5cfec64a26c99488
corawym <corawym@gmail.com>
2017-07-20 15:14:47 -0700

    added gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 // Requiring packages
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
-    watch = require('gulp-watch');
-    browserSync = require('browser-sync').create();
-    eslint = require('gulp-eslint');
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    prettyError = require('gulp-prettyerror');
      
 
 // Gulp Tasks Below
 
 // Gulp Scripts Task
-gulp.task('scripts', function() {
+gulp.task('scripts', ['lint'], function() {
    gulp.src('./js/*.js') // What files do we want gulp to consume?
     .pipe(uglify()) // Call the uglify function on these files
     .pipe(rename({ extname: '.min.js' })) // Rename the uglified file
     .pipe(gulp.dest('./build/js')) // Where do we put the result?
+});
+
+// Gulp sass function
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
 });
 
 //Reload browser
@@ -25,12 +43,13 @@ gulp.task('browser-sync', function() {
         }
     }); //End of browser sync init
 
-    gulp.watch(['./build/js/*.js','./index.html','./css/style.css']).on('change', browserSync.reload);
+    gulp.watch(['./build/js/*.js','./index.html','./build/css/*.css']).on('change', browserSync.reload);
 });
 
 // Gulp watch function
 gulp.task('watch', function() {
    gulp.watch('./js/*.js', ['scripts']);
+   gulp.watch('./sass/*.scss', ['sass']);
 });
 
 //Gulp lint function
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   // Built by LucyBot. www.lucybot.com
+  
+
   var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
       url += '?' + $.param({
         'api-key': 'ca8f3034cfb64fe8acb5a2a193f4c8c3'
@@ -12,5 +14,6 @@ $(document).ready(function(){
   }).fail(function(err) {
     throw err;
   });
+
 
 });

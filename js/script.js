54ab2f161d40bde896d277f332bcb334a062e1a2
corawym <corawym@gmail.com>
2017-07-20 23:31:31 -0700

    Used Ajax to get data from the NYT Top Stories API

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
-  // Built by LucyBot. www.lucybot.com
-  
-
-  var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
-      url += '?' + $.param({
-        'api-key': 'ca8f3034cfb64fe8acb5a2a193f4c8c3'
+  // Select a section function
+  $('#sections').on('change', function() {
+    var sectionName = $(this).val();
+    var url = 'https://api.nytimes.com/svc/topstories/v2/';
+        url += sectionName;
+        url += '.json';
+        url += '?' + $.param({'api-key': 'ca8f3034cfb64fe8acb5a2a193f4c8c3'
+                });
+    // use Ajax to get data from the NYT Top Stories API
+    $.ajax({
+      url: url,
+      method: 'GET'
+    }).done(function(result) {
+      // get the abstract, url and photo of a story
+      $.each(result.results, function( key, value ){
+        var storyAbstract = value.abstract;
+        var storyUrl = value.short_url;
+        var storyPhoto = value.multimedia[4].url;
       });
-  $.ajax({
-    url: url,
-    method: 'GET'
-  }).done(function(result) {
-    console.log(result);
-  }).fail(function(err) {
-    throw err;
-  });
+    }).fail(function(err) {
+      throw err;
+    });
+
+  }); //end of select a section function
 
 
 });

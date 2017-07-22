c3254d907a41f02a18261ac5327fa178b926cc93
corawym <corawym@gmail.com>
2017-07-21 23:45:33 -0700

    loaded the stories in mobile view

--- js/script.js
+++ js/script.js
@@ -7,17 +7,30 @@ $(document).ready(function(){
         url += '.json';
         url += '?' + $.param({'api-key': 'ca8f3034cfb64fe8acb5a2a193f4c8c3'
                 });
+
     // use Ajax to get data from the NYT Top Stories API
     $.ajax({
       url: url,
       method: 'GET'
     }).done(function(result) {
+
       // get the abstract, url and photo of a story
       $.each(result.results, function( key, value ){
+
         var storyAbstract = value.abstract;
-        var storyUrl = value.short_url;
+        var storyUrl = value.url;
         var storyPhoto = value.multimedia[4].url;
+         
+        var style = "background-image:url('" + storyPhoto + "')";
+        var storyListItem = '<li class="story-list-item"><a href="' + storyUrl + '" target="_blank">';
+            storyListItem += '<div class="story-list-bgphoto" style="' + style + '"><p class="story-list-text">';
+            storyListItem += storyAbstract;
+            storyListItem += '</p></div></a></li>';
+
+
+        $('.story-list').append(storyListItem);
       });
+
     }).fail(function(err) {
       throw err;
     });
@@ -26,3 +39,4 @@ $(document).ready(function(){
 
 
 });
+

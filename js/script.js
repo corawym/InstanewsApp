96cf9f08d0d4be3825d4caa8cafb830b40ecb79b
corawym <corawym@gmail.com>
2017-07-23 02:09:12 -0700

    Only display max. twelve stories with a photo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   // Select a section function
   $('#sections').on('change', function() {
+    // remove the previous select option
+    $('.story-list-item').remove();
+
+    // use Ajax to get data from the NYT Top Stories API
     var sectionName = $(this).val();
     var url = 'https://api.nytimes.com/svc/topstories/v2/';
         url += sectionName;
@@ -8,35 +12,42 @@ $(document).ready(function(){
         url += '?' + $.param({'api-key': 'ca8f3034cfb64fe8acb5a2a193f4c8c3'
                 });
 
-    // use Ajax to get data from the NYT Top Stories API
     $.ajax({
       url: url,
       method: 'GET'
     }).done(function(result) {
-
-      // get the abstract, url and photo of a story
+      var allTwelveStoriesWithImages = [];
       $.each(result.results, function( key, value ){
-
-        var storyAbstract = value.abstract;
-        var storyUrl = value.url;
-        var storyPhoto = value.multimedia[4].url;
-         
-        var style = "background-image:url('" + storyPhoto + "')";
-        var storyListItem = '<li class="story-list-item"><a href="' + storyUrl + '" target="_blank">';
-            storyListItem += '<div class="story-list-bgphoto" style="' + style + '"><p class="story-list-text">';
-            storyListItem += storyAbstract;
-            storyListItem += '</p></div></a></li>';
-
-
-        $('.story-list').append(storyListItem);
-      });
-
+        // only get a story if it has a photo and get maximum of 12 stories in an array
+        if (value.multimedia.length>0 && allTwelveStoriesWithImages.length<12){
+          allTwelveStoriesWithImages.push(value);
+        }
+      }); // end of each function
+      // display the stories in grid
+      if (allTwelveStoriesWithImages.length>0){
+        $.each(allTwelveStoriesWithImages, function( key, value ){
+          // get the abstract, url and photo of a story
+          var storyAbstract = value.abstract;
+          var storyUrl = value.url;
+          var storyPhoto = value.multimedia[4].url;
+          
+          var style = "background-image:url('" + storyPhoto + "')";
+          var storyListItem = '<li class="story-list-item"><a href="' + storyUrl + '" target="_blank">';
+              storyListItem += '<div class="story-list-bgphoto" style="' + style + '"><p class="story-list-text">';
+              storyListItem += storyAbstract;
+              storyListItem += '</p></div></a></li>';
+        
+          $('.story-list').append(storyListItem);
+        }); // end of each function
+      }else{
+        $('.story-list').append('<li><p>Sorry, no stories are updated.</p></li>');
+      }
+        
     }).fail(function(err) {
       throw err;
-    });
+    }); // end of fail function
 
-  }); //end of select a section function
+  }); // end of on change function
 
-
-});
+}); // end of document ready function
 

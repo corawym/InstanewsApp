957bd156865aa1caf429cc1947d78a9448e40df8
corawym <corawym@gmail.com>
2017-07-24 19:31:06 -0700

    cleaned up

--- js/script.js
+++ js/script.js
@@ -6,10 +6,8 @@ $(document).ready(function(){
 
   // Select a section function
   $('#sections').on('change', function() {
-    
     var sectionName = $(this).val();
     if (sectionName !== ''){
-
       //  change the size of header
       $('.site-header').addClass('site-header-small');
       $('.story-grid').show();
@@ -17,16 +15,13 @@ $(document).ready(function(){
       $('body').removeClass('container-vh');
       $('.story-list-item').remove();
       $('.error-msg').remove();
-
       $('.site-footer').hide();
       $('.loader').show();
 
       var url = 'https://api.nytimes.com/svc/topstories/v2/';
           url += sectionName;
           url += '.json';
-          url += '?' + $.param({'api-key': 'ca8f3034cfb64fe8acb5a2a193f4c8c3'
-                  });
-      
+          url += '?' + $.param({'api-key':'ca8f3034cfb64fe8acb5a2a193f4c8c3'});
       // use Ajax to get data from the NYT Top Stories API
       $.ajax({
         url: url,
@@ -46,7 +41,7 @@ $(document).ready(function(){
             var storyAbstract = value.abstract;
             var storyUrl = value.url;
             var storyPhoto = value.multimedia[4].url;
-            
+  
             var style = "background-image:url('" + storyPhoto + "')";
             var storyListItem = '<li class="story-list-item"><a href="' + storyUrl + '" target="_blank">';
                 storyListItem += '<div class="story-list-bgphoto" style="' + style + '"><p class="story-list-text">';
@@ -59,7 +54,6 @@ $(document).ready(function(){
           $('body').addClass('container-vh');
           $('.story-list').append('<li class="error-msg"><p>Sorry, no stories are updated.</p></li>');
         }
-
       }).fail(function(err) {
         $('.loader').hide();
         $('body').addClass('container-vh');
@@ -71,8 +65,6 @@ $(document).ready(function(){
         $('.site-footer').show();  
       }); // end of ajax function
     } // end of sectionName is not empty
-    
   }); // end of on change function
-
 }); // end of document ready function
 

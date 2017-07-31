b79036bbdff9c9eb71e91e1bc872fa29e4e3cdb9
corawym <corawym@gmail.com>
2017-07-31 16:20:43 -0700

    refactored js and used ES.next syntax

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
+$(() => {
   // jQuery Selectric
-  $(function() {
-    $('select').selectric();
-  });
+  $('select').selectric();
 
   // Select a section function
-  $('#sections').on('change', function() {
-    var sectionName = $(this).val();
+  $('#sections').on('change', (ev) => {
+    let sectionName = $(ev.currentTarget).val();
     if (sectionName !== ''){
       //  change the size of header
       $('.site-header').addClass('site-header-small');
@@ -18,17 +16,14 @@ $(document).ready(function(){
       $('.site-footer').hide();
       $('.loader').show();
 
-      var url = 'https://api.nytimes.com/svc/topstories/v2/';
-          url += sectionName;
-          url += '.json';
-          url += '?' + $.param({'api-key':'ca8f3034cfb64fe8acb5a2a193f4c8c3'});
+      let url = `https://api.nytimes.com/svc/topstories/v2/${sectionName}.json?${$.param({'api-key':'ca8f3034cfb64fe8acb5a2a193f4c8c3'})}`;
       // use Ajax to get data from the NYT Top Stories API
       $.ajax({
         url: url,
         method: 'GET'
-      }).done(function(result) {
-        var allTwelveStoriesWithImages = [];
-        $.each(result.results, function( key, value ){
+      }).done((result) => {
+        const allTwelveStoriesWithImages = [];
+        $.each(result.results, (key, value) => {
           // only get a story if it has a photo and get maximum of 12 stories in an array
           if (value.multimedia.length>0 && allTwelveStoriesWithImages.length<12){
             allTwelveStoriesWithImages.push(value);
@@ -36,17 +31,13 @@ $(document).ready(function(){
         }); // end of each function
         // display the stories in grid
         if (allTwelveStoriesWithImages.length>0){
-          $.each(allTwelveStoriesWithImages, function( key, value ){
-            // get the abstract, url and photo of a story
-            var storyAbstract = value.abstract;
-            var storyUrl = value.url;
-            var storyPhoto = value.multimedia[4].url;
-  
-            var style = "background-image:url('" + storyPhoto + "')";
-            var storyListItem = '<li class="story-list-item"><a href="' + storyUrl + '" target="_blank">';
-                storyListItem += '<div class="story-list-bgphoto" style="' + style + '"><p class="story-list-text">';
-                storyListItem += storyAbstract;
-                storyListItem += '</p></div></a></li>';
+          $.each(allTwelveStoriesWithImages, (key, value) => {
+            // get the abstract, url and photo of a story  
+            let storyListItem = `<li class='story-list-item'>
+                                  <a href='${value.url}' target='_blank'>
+                                    <div class='story-list-bgphoto' style='background-image:url("${value.multimedia[4].url}")'>
+                                      <p class='story-list-text'>${value.abstract}</p>
+                                  </div></a></li>`;
             
             $('.story-list').append(storyListItem);
           }); // end of each function
@@ -54,13 +45,13 @@ $(document).ready(function(){
           $('body').addClass('container-vh');
           $('.story-list').append('<li class="error-msg"><p>Sorry, no stories are updated.</p></li>');
         }
-      }).fail(function(err) {
+      }).fail((err) => {
         $('.loader').hide();
         $('body').addClass('container-vh');
         $('.story-list').append('<li class="error-msg"><p>Internal Server Error</p></li>');
         $('.site-footer').show(); 
         throw err;
-      }).always(function(){
+      }).always(() => {
         $('.loader').hide();
         $('.site-footer').show();  
       }); // end of ajax function
